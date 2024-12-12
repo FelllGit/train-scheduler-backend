dd1486aa0231cfd8e2db43c3b508bb2f4b8c1892
Danylo Paliienko <it18@eurocommerce.biz>
2024-12-12 09:45:10 +0200

    removed request to db

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,25 +1,19 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private usersService: UsersService) {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate(payload: any) {
-    const user = await this.usersService.findOneById(payload.id);
-
-    if (!user) {
-      throw new UnauthorizedException('User not found');
-    }
-
-    return { userId: user.id, email: user.email };
+    return { userId: payload.sub, email: payload.email };
   }
 }
